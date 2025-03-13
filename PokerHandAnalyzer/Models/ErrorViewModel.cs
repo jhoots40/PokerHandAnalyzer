9be9de3a8131048b146b713e48a950e906dd84a7
jhoots40 <jake.houts.5@gmail.com>
2025-03-13 11:23:19 -0500

    Initial Project Setup

---
+++ PokerHandAnalyzer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PokerHandAnalyzer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
